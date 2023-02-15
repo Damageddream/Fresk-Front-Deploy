ce9d583353ba83d4b1ce0a88990b58da66325dce
unknown <marcan.grab@gmail.com>
2023-02-15 13:15:11 +0100

    fixed responsivness for GaleriaSali

--- src/components/Co Robimy/Uslugi.js
+++ src/components/Co Robimy/Uslugi.js
@@ -16,7 +16,7 @@ const Uslugi = () => {
       <Jumbotron title={opis.title} photo={opis.photo} />
       <Row>
         <Col className='m-5'>
-        <h2 className='text-center'>Wynajem sali</h2>
+          <h2 className='text-center'>Wynajem sali</h2>
           < br />
           <p>Sala konferencyjna i szkoleniowa Fundacji Rozwoju Edukacji i Społecznej Kreatywności zlokalizowana jest w budynku przy ul. Noniewicza 34 w Suwałkach jest przeznaczona do prowadzenia szkoleń, spotkań, konferencji i pokazów. Rozmiar przestrzeni pozwala również na swobodne organizowanie innych podobnych wydarzeń.</p>
           <p>Dzięki dostępowi do Internetu oraz kompleksowemu oświetleniu sala nadaję się do prowadzenia webinariów oraz zajęć zdalnych. </p>
@@ -24,10 +24,12 @@ const Uslugi = () => {
           <p>Sala wyposażona jest w 6 stołów, które można swobodnie łączyć i rozdzielać dzięki czemu mogą odbywać się w niej szkolenia grupowe, indywidualne, konferencje oraz spotkania biznesowe. Do dyspozycji jest 12 miejsc siedzących dla uczestników + miejsce dla prowadzącego. Sala jest dostępna 7 dni w tygodniu 24 godziny na dobę</p>
           <p>Koszt 70 zł na godzinę. </p>
         </Col>
-        <Col className='m-5'>
-          <div className='mt-5'>
-            <GaleriaSali />
-          </div>
+        <Col className='mt-5'>
+          <Row>
+            <div className='mt-5'>
+              <GaleriaSali />
+            </div>
+          </Row>
         </Col>
       </Row>
     </>
